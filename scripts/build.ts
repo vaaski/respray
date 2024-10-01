1c1b761ede1c644a37fea39427473787bcc744ff
vaaski <admin@vaa.ski>
2024-10-01 21:35:26 +0200

    use esbuild for building

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,35 @@
-await Bun.build({
-  entrypoints: ["src/index.ts"],
-  outdir: "dist",
-  target: "node",
+import esbuild from "esbuild"
+import { parseArgs } from "node:util"
+
+const args = parseArgs({
+  options: {
+    dev: {
+      type: "boolean",
+      short: "d",
+    },
+  },
 })
+
+const esbuildConfig: esbuild.BuildOptions = {
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  outdir: "dist",
+  format: "esm",
+  platform: "node",
+  minify: true,
+
+  banner: {
+    js: "#!/usr/bin/env node",
+  },
+}
+
+if (args.values.dev) {
+  const ctx = await esbuild.context(esbuildConfig)
+
+  console.log("watching...")
+
+  await ctx.watch()
+} else {
+  await esbuild.build(esbuildConfig)
+  console.log(`built ${esbuildConfig.outfile}`)
+}
