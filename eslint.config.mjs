b6d6c01d7c2cbe1d01fdbd79137656651149ef07
Oskar Sommer <oskar.sommer@t-systems.com>
2024-10-14 13:23:28 +0200

    respray

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tsEslint from "typescript-eslint"
+import unicorn from "eslint-plugin-unicorn"
+
+export default tsEslint.config(
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tsEslint.configs.recommended,
+  unicorn.configs["flat/recommended"],
+)
