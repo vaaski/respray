6939b0e15f77c95f3c33b2b10b6baefc759b140d
Oskar Sommer <oskar.sommer@t-systems.com>
2025-01-01 18:06:29 +0100

    update resprayed eslint configs

--- configs/eslint-nuxt.js
+++ configs/eslint-nuxt.js
@@ -2,24 +2,26 @@ import withNuxt from "./.nuxt/eslint.config.mjs"
 import unicorn from "eslint-plugin-unicorn"
 
 export default withNuxt([
-	{
-		files: ["**/*.html", "**/*.vue"],
-		rules: {
-			// this is stupid.
-			// https://github.com/prettier/prettier/issues/15336
-			"vue/html-self-closing": "off",
-		},
-	},
-]).prepend([
 	unicorn.configs["flat/recommended"],
 	{
 		rules: {
 			"unicorn/prefer-ternary": "off",
+			"unicorn/prefer-global-this": "off",
+		},
+	},
+	{
+		files: ["utils/*.ts", "functions/*.mts"],
+		rules: {
+			"unicorn/no-anonymous-default-export": "off",
 		},
 	},
 	{
 		files: ["**/*.vue"],
 		rules: {
+			// this is stupid.
+			// https://github.com/prettier/prettier/issues/15336
+			"vue/html-self-closing": "off",
+			"vue/component-name-in-template-casing": ["error", "PascalCase"],
 			"unicorn/prevent-abbreviations": [
 				"error",
 				{
@@ -28,7 +30,6 @@ export default withNuxt([
 					},
 				},
 			],
-			"vue/component-name-in-template-casing": ["error", "PascalCase"],
 		},
 	},
 ])
